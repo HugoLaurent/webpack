d93fbd50cbe76d5274ce73e75127ac0eb7eeda27
HugoLaurent <lauhugopro@gmail.com>
2024-05-21 14:48:14 +0200

    feat: config webpack

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -11,6 +11,29 @@ const config: Configuration = {
         publicPath: '/',
     },
     entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.{ts|jsx}x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel@/preset.env','@babel/preset-typescript', '@babel/preset-react'],
+                    },
+                },
+            }           
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new HotModuleReplacementPlugin(),
+    ],
 };
 
 export default config;
